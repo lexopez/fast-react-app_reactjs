b717e2e5db5f5f3b409220f9fbdba0ee3d86f42a
alexander lopez <iamlexlopez@gmail.com>
2025-12-23 11:45:16 +0800

    Update SearchOrder.jsx

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
 
 function SearchOrder() {
-    function handleSubmit(){}
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+    function handleSubmit(e){
+      e.preventDefault();
+      if(!query) return;
+      navigate(`/order/${query}`);
+      setQuery('')
+    }
+
     return (
     <form onSubmit={handleSubmit}>
       <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="Search order #"
         className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
