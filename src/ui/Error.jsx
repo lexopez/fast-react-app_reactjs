f9906711287d3e06cf674dc94f120fbd232f44f6
alexander lopez <iamlexlopez@gmail.com>
2025-12-23 11:56:24 +0800

    update error component

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,13 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
+import LinkButton from './LinkButton';
 
 function NotFound() {
-  const navigate = useNavigate();
+  const error = useRouteError();
+  console.log(error)
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <p>{error.date || error.message}</p>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
+      
     </div>
   );
 }
