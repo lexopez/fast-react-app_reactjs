1cdbaa65fc0e964d65ed8eb4b086179e70c689a5
alexander lopez <iamlexlopez@gmail.com>
2025-12-23 12:13:32 +0800

    create loader component
    check the state of navigation if it is true then render this component.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigate = useNavigation();
+  const isLoading = navigate.state === 'loading';
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto">
+      {isLoading && <Loader />}
       <Header />
 
       <div className="overflow-scroll">
